28b6ee702b8a1fb1f084bf9c0559704dbac60b79
Bhuvan0811 <105094547+Bhuvan0811@users.noreply.github.com>
2024-06-04 12:11:43 +0530

    changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const rootRouter = require("./routes/index");
 const app = express();
@@ -8,5 +9,6 @@ const JWT_secret = require("./config");
 app.use(cors());
 app.use(bodyParser.json());
 
+const port = process.env.PORT || 8000;
 app.use("/api/v1", rootRouter);
-app.listen(8000);
+app.listen(port);
